e9c6451a7e2315132fea8f015d943fd87144cae2
pawan kumar <pawankumar1912005@gmail.com>
2026-09-19 14:30:38 +0530

    Time: 74 ms (15.87%) | Memory: 94 MB (68.19%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end());
+
+        int remove = 0;
+        int prevEnd = intervals[0][1];
+
+        for(int i = 1; i < intervals.size(); i++) {
+
+            if(intervals[i][0] >= prevEnd) {
+            
+                prevEnd = intervals[i][1];
+            }
+            else {
+                
+                remove++;
+
+                
+                prevEnd = min(prevEnd, intervals[i][1]);
+            }
+        }
+
+        return remove;
+    }
+};
