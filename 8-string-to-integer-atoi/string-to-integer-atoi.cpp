14aeb1cc800ab3a79313fd6398626daabc0caf98
pawan kumar <pawankumar1912005@gmail.com>
2026-09-24 22:39:09 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (53.46%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+      int i = 0;
+        int n = s.length();
+
+        
+        while (i < n && s[i] == ' ') {
+            i++;
+        }
+
+    
+        int sign = 1;
+
+        if (i < n && (s[i] == '+' || s[i] == '-')) {
+            if (s[i] == '-') {
+                sign = -1;
+            }
+            i++;
+        }
+
+    
+        long long num = 0;
+
+        while (i < n && isdigit(s[i])) {
+            int digit = s[i] - '0';
+
+            num = num * 10 + digit;
+
+            
+            if (num * sign > INT_MAX) {
+                return INT_MAX;
+            }
+
+            if (num * sign < INT_MIN) {
+                return INT_MIN;
+            }
+
+            i++;
+        }
+
+        return num * sign;
+    }
+};
